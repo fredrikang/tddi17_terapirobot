2c75708817cdde293524527492889bf53e7e76dc
jerzky <jensg90@gmail.com>
2020-10-01 15:38:55 +0200

    uupdated the streamfeed thingy

--- ExternalScripts/CameraWatcher/watchcamera.py
+++ ExternalScripts/CameraWatcher/watchcamera.py
@@ -1,30 +1,41 @@
 import cv2
 import zmq
-import base64
 import io
 import numpy as np
-from PIL import Image 
-context = zmq.Context()
-footage_socket = context.socket(zmq.SUB)
-footage_socket.connect('tcp://192.168.43.131:3000')
-footage_socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
-while True:
-    try:
-        frame = footage_socket.recv()
-        imgBuff = np.frombuffer(frame, dtype=np.uint8)
-        print(imgBuff)
-        img = cv2.imdecode(imgBuff, cv2.IMREAD_COLOR)
-        
-        scale_percent = 200
-        width = int(img.shape[1] * scale_percent / 100)
-        height = int(img.shape[0] * scale_percent / 100)
-        dim = (width, height)
-        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-        
-        cv2.imshow("image", resized)
-        cv2.waitKey(1)
 
-    except KeyboardInterrupt:
-        cv2.destroyAllWindows()
-        break
+class FurHatStream:
+    def __init__(self, size, record):
+        self.size = size
+        self.record = record
+        context = zmq.Context()
+     
+        self.footage_socket = context.socket(zmq.SUB)
+        self.footage_socket.connect('tcp://192.168.43.131:3000')
+        self.footage_socket.setsockopt_string(zmq.SUBSCRIBE, "")
+        if record:
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            self.video_writer = cv2.VideoWriter("output.avi", fourcc, 10, self.size)
+       
+    def run(self):
+        while True:
+            try:
+                frame = self.footage_socket.recv()
+                imgBuff = np.frombuffer(frame, dtype=np.uint8)
+                print(imgBuff)
+                img = cv2.imdecode(imgBuff, cv2.IMREAD_COLOR)
+                img = cv2.resize(img, self.size, interpolation = cv2.INTER_AREA)
+                if self.record:
+                    self.video_writer.write(img)
+                cv2.imshow("FurHatStream", img)
+                cv2.waitKey(1)
+
+            except KeyboardInterrupt:
+                if self.record:
+                    self.video_writer.release()
+                cv2.destroyAllWindows()  
+                break
+
+
+stream = FurHatStream(size = (800, 600), record = False)
+stream.run()
