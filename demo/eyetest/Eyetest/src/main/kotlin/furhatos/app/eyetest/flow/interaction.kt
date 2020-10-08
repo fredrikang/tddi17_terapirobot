ba2636c74dffa770b65d9953aaa8ce0f2f1fb8dc
FredrikAngstrom <fredrik.ang@gmail.com>
2020-10-08 15:01:59 +0200

    added rudimentary appearance selection to demo/eyetest

--- demo/eyetest/Eyetest/src/main/kotlin/furhatos/app/eyetest/flow/interaction.kt
+++ demo/eyetest/Eyetest/src/main/kotlin/furhatos/app/eyetest/flow/interaction.kt
@@ -1,5 +1,7 @@
 package furhatos.app.eyetest.flow
 
+import furhatos.app.eyetest.nlu.Nr1Intent
+import furhatos.app.eyetest.nlu.Nr2Intent
 import furhatos.nlu.common.*
 import furhatos.flow.kotlin.*
 import furhatos.gestures.Gestures
@@ -8,12 +10,17 @@ import furhatos.nlu.common.Yes
 import furhatos.nlu.common.No
 import furhatos.records.User
 import furhatos.skills.UserManager
-
+import furhatos.util.Language
+import furhatos.util.Gender
+import furhatos.flow.kotlin.voice.Voice
 
 val Start : State = state(Interaction) {
-    /* init{
-        users.engagementPolicy = (SingleUserEngagementPolicy())
-    } */
+     init{
+        //furhat.setVoice(Language.SWEDISH, Gender.FEMALE, true)
+        furhat.voice = Voice(gender = Gender.FEMALE, language = Language.SWEDISH, pitch = "high")
+        furhat.setInputLanguage(Language.SWEDISH)
+        furhat.setTexture("Angelina")
+     }
 
     /*  onEntry to make sure we ask all users in view of furhat if they are the intended patient.
         Users who have been establish to not be patients are marked with a diregard boolean. So that they can be ignored.
@@ -23,9 +30,9 @@ val Start : State = state(Interaction) {
             for ( it in users.list){
                 if (!it.disregard) {
                     furhat.attend(it)
-                    val resp = call(findTargetUser(it, "Hello! Do you want to speak to me?", "Okay nice to meet you!", "Okay, I won't speak to you.")) as Boolean
+                    val resp = call(findTargetUser(it, "Hej! Vill du prata med mig?", "Okej, trevligt att träffas!", "Okej, då pratar jag inte med dig.")) as Boolean
                     if (resp) {
-                        goto(Test)
+                        goto(Appearance)
                     }
                     else {
                         furhat.attendNobody() // If no user can be established as the patient furhat will ignore all users.
@@ -38,9 +45,9 @@ val Start : State = state(Interaction) {
     onUserEnter(instant = true) {
         if (!it.disregard){
             furhat.attend(it)
-            val resp = call(findTargetUser(it, "Hello! Do you want to speak to me?", "Okay nice to meet you!", "Okay, I won't speak to you.")) as Boolean
+            val resp = call(findTargetUser(it, "Hej! Vill du prata med mig?", "Okej, trevligt att träffas!", "Okej, då pratar jag inte med dig.")) as Boolean
             if (resp) {
-                goto(Test)
+                goto(Appearance)
             }
             else {
                 furhat.attendNobody()
@@ -77,21 +84,49 @@ fun findTargetUser(user: User, stringAsk: String, stringYes: String, stringNo: S
     }
 }
 
+
+val Appearance : State = state(Interaction) {
+    onEntry {
+        furhat.say({+"Jag kan ta flera olika utseenden. Jag kan se ut så här."
+                    +delay(2000)})
+        furhat.voice = Voice(gender = Gender.FEMALE, language = Language.SWEDISH, pitch = "low")
+        furhat.setInputLanguage(Language.SWEDISH)
+        furhat.setTexture("Arianna")
+        furhat.say("Jag kan också se ut så här.")
+        furhat.ask("Vilket föredrar du, det första eller det andra?")
+    }
+
+    onResponse<Nr1Intent> {
+        furhat.voice = Voice(gender = Gender.FEMALE, language = Language.SWEDISH, pitch = "high")
+        furhat.setTexture("Angelina")
+        goto(Test)
+    }
+
+    onResponse<Nr2Intent> {
+        furhat.voice = Voice(gender = Gender.FEMALE, language = Language.SWEDISH, pitch = "low")
+        furhat.setInputLanguage(Language.SWEDISH)
+        goto(Test)
+    }
+
+    onExit {
+        furhat.say("Okej, då tar jag det här utseendet.")
+    }
+}
 val Test : State = state(Interaction) {
     onEntry {
-        furhat.say("Entered Test state.")
-        furhat.say("Current user is.")
+        furhat.say("Gick in i teststadiet.")
+        furhat.say("Den nuvarande användaren är.")
         furhat.say(users.targetUser) // For debugging purposes only.
     }
 
 /*
         If the targetUser has been lost and a new user enters we once again check if the User is our patient.
-        If the we are still focused on the tartget user we simply glance at the new user.
+        If the we are still focused on the target user we simply glance at the new user.
  */
     onUserEnter(instant = true) {
         if ( !users.hasTargetUser ) {
             furhat.attend(it)
-            val resp = call(findTargetUser(it, "Are you my patient?", "Great let's continue.", "Okay I understand.")) as Boolean
+            val resp = call(findTargetUser(it, "Är du min patient?", "Bra! Då fortsätter vi.", "Okej, jag förstår.")) as Boolean
             if (!resp) {
                 furhat.attendNobody()
             }
@@ -109,11 +144,11 @@ val Test : State = state(Interaction) {
             users.hasTargetUser = false
             users.targetUser = "None"
             furhat.attendNobody()
-            furhat.say ( "Lost target user." )
+            furhat.say ( "Huvudanvändare borta." )
 
         }
         else {
-            furhat.say("Other user left.") //Purely for debugging purposes.
+            furhat.say("Annan användare borta.") //Purely for debugging purposes.
         }
     }
 }
--- demo/eyetest/Eyetest/src/main/kotlin/furhatos/app/eyetest/nlu/nlu.kt
+++ demo/eyetest/Eyetest/src/main/kotlin/furhatos/app/eyetest/nlu/nlu.kt
@@ -1,1 +1,28 @@
 package furhatos.app.eyetest.nlu
+
+import furhatos.nlu.Intent
+import furhatos.util.Language
+
+class Nr1Intent : Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+            "nummer ett",
+            "det första",
+            "första",
+            "ettan",
+            "alternativ ett"
+        )
+    }
+}
+
+class Nr2Intent : Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+                "nummer två",
+                "det andra",
+                "andra",
+                "tvåan",
+                "alternativ två"
+        )
+    }
+}
