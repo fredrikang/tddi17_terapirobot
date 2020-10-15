df2b0fb8b7076578cb2103b2a431a24354053f64
Tim <timcarsting@gmail.com>
2020-10-15 14:37:11 +0200

    furhatui start

--- UI/furhatui.py
+++ UI/furhatui.py
@@ -1,11 +1,48 @@
+#!/usr/bin/python
 import socket
+import opencvwindow
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import  QWidget, QLabel, QApplication, QVBoxLayout, QPushButton
+from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
+import cv2
 import sys
 
+class App(QApplication):
+    def __init__(self, stringArray):
+        super().__init__(stringArray)
+        self.initLayout()
+        self.initWindow()
 
-class FurhatTCPConnection:
-    def __init__(self, host):
-        self.socket_address = (host, 1982)
-        print >>sys.stderr, 'starting up on %s port %s'
-    
+    def initLayout(self):
+        self.layout = QVBoxLayout()
+
+    def initWindow(self):
+        self.window = QWidget()
+        self.window.setLayout(self.layout)
+
+    @pyqtSlot(QWidget)
+    def addWidget(self, widget):
+        self.layout.addWidget(widget)
+
+    def addWidgets(self, widgets):
+        for w in widgets:
+            self.addWidget(w)
+
     def run(self):
-        self.sock.bind(server_address)
+        self.window.show()
+        super().exec_()
+
+class Button(QPushButton):
+    def __init__(self, text):
+        super().__init__(text)
+
+
+
+
+app = App([])
+cvVideo = opencvwindow.OpenCVWindow()
+button = Button("Hej")
+app.addWidget(button)
+app.addWidget(cvVideo)
+app.run()
