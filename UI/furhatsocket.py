aeba0d098b8819576378a58ad4edfba53f2f33d1
jerzky <jensg90@gmail.com>
2020-10-15 12:55:47 +0200

    added reqs

--- UI/furhatsocket.py
+++ UI/furhatsocket.py
@@ -5,7 +5,7 @@ import sys
 class FurhatTCPConnection:
     def __init__(self, host):
         self.socket_address = (host, 1982)
+        self.socket = .bind(server_address)
         print >>sys.stderr, 'starting up on %s port %s'
     
     def run(self):
-        self.sock.bind(server_address)
