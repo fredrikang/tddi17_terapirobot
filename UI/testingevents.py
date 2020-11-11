d5213d4b99bf8253a9e1c0e9c77cdabd353e5f81
jerzky <jensg90@gmail.com>
2020-11-11 14:45:05 +0100

    added new event and updated the python reqs

--- UI/testingevents.py
+++ UI/testingevents.py
@@ -9,10 +9,8 @@ def Test(data):
 
 furhat = FurhatInterface("TestingFurhat", "192.168.43.131")
 furhat.subscribe("furhatos.event.senses.SenseUsers", Test)
+
 #furhat.subscribe("**", Test)
-while 1:
-    #furhat.speak(input())
-    for r in range(255):
-        for g in range(255):
-            for b in range(255):
-                furhat.led(r,g,b)
+while True:
+    input()
+    furhat.changemode()
